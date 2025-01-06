4210adfd11add9943fdf5b297121ebae2087646d
Floki-1 <mobeenjamil.1990@gmail.com>
2025-01-06 16:15:29 +0100

    Hej jens 16:15

---
+++ labb4/Labb4/Labb4/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Labb4
+{
+
+   public enum Gender
+    {
+        Male,             
+        Female,         
+        Nonbinary,      
+
+    }
+
+    public struct Hair()
+    {
+        public string Color { get; set; }
+        public string Length;
+
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string EyesColor;
+        public Hair Info { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+
+
+        
+        public override string ToString()
+        {
+            return $"Ögonfärg {EyesColor}, Ålder {BirthDate}, name {Name}, kön {Gender}";
+        }
+    }
+}
---
+++ labb4/Labb4/Labb4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.IO.Pipes;
+using System.Threading.Channels;
+
+namespace Labb4
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            
+                
+            
+            var person = new Person();
+            /*Hair PersonHair  = new Hair();*/
+
+            person.EyesColor = "blue ";
+            person.Name = "jens";
+            person.BirthDate = DateTime.Now;
+            person.Gender=Gender.Male;
+            person.Info = new Hair { Color = "Brown", Length = "10 m" };
+
+
+
+            
+
+            List<Person> People= new List<Person>();
+            Boolean KeepGoing = true;
+            while (KeepGoing)
+            {
+                
+            }
+
+             People.Add(new Person
+             {
+
+             });
+            
+                
+
+            
+
+
+
+            Console.WriteLine(person.ToString());
+            Console.WriteLine($"Hair Color: {person.Info.Color}");
+            Console.WriteLine($"Hair Style: {person.Info.Length}");
+            Console.WriteLine(person.EyesColor + person.Name);
+            Console.WriteLine(person.BirthDate);
+            Console.WriteLine(person.Gender);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        
+
+       
+
+
+       
+
+                
+
+
+
+        
+    }
+}
